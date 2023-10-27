26652b4864d61c67f6f467830ec6e547d93a8002
satosakisato <121882046+satosakisato@users.noreply.github.com>
2023-10-27 14:11:33 +0200

    Misa a jour Drangon_ball.php
    fini system de combat, amélioration de l'apparence (fluide et épurer)

--- Drangon_ball.php
+++ Drangon_ball.php
@@ -42,13 +42,18 @@ class hero extends Personage {
         //attack the enemy
         print("Tu attaque l'enemy avec ". $this->technique[key($this->technique)] ."\n");
         print("L'enemy prend ".$enemy->prendredegat($this->Attack*key($this->technique))." de degats.\n");
+        sleep(2);
+        popen('cls','w');
         $enemy->arreterdesedeffendre();
         reset($this->technique);
         return $this->Attack*key($this->technique);
     }
 
     public function arreterdesedeffendre(){
-        $this->sedefendre=false;
+        if ($this->sedefendre==true){
+            $this->Defence= $this->Defence/2;
+            $this->sedefendre=false;
+        }
     }
 
     public function prendredegat($attackEnemy){
@@ -76,6 +81,10 @@ class hero extends Personage {
         //increase the defence
         $this->Defence=$this->Defence*2;
         $this->sedefendre=true;
+        print("Vous vous dÃ©fendez!\n");
+        print("Defence X2!\n");
+        sleep(2);
+        popen('cls','w');
     }
     public function afficherStat(){
         print("Voici les statistique de ".$this->Nom.":\n");
@@ -83,12 +92,37 @@ class hero extends Personage {
         print("Attaque:".$this->Attack."\n");
         print("Defence:".$this->Defence."\n");
         print("Vitesse:".$this->Vitesse."\n");
-        print("Vie:".$this->Vie."/".$this->VieMax."\n");
+        print("Vie Max: ".$this->VieMax."\n");
+        print("Barre de vie :[");
+        for ($i=0; $i <round($this->Vie/$this->VieMax*20) ; $i++) { 
+            print("|");
+        }
+        for ($i=0; $i <20-round($this->Vie/$this->VieMax*20) ; $i++) { 
+            print(".");
+        }
+        print("]\n");
     }
 
     public function getnbrdevictoire(){
         return $this->nbrdevictoire;
     }
+
+    public function Victoire(){
+        $this->nbrdevictoire= $this->nbrdevictoire+1;
+        $this->Attack= $this->Attack+5;
+        $this->Defence= $this->Defence+5;
+        $this->Vitesse= $this->Vitesse+5;
+        $this->VieMax= $this->VieMax+25;
+        print("+5 attack, defence et vitesse\n +25 Vie max\n");
+    }
+
+    public function addtechnique($technique){
+        $this->technique[key($technique)]= $technique[key($technique)];
+    }
+    public function Fullvie(){
+        $this->Vie= $this->VieMax;
+    }
+
 }
 
 // A class with stats for the villain.
@@ -97,6 +131,9 @@ class villain extends Personage{
     public function __construct($n, $v,$vmax, $p, $a, $d, $vi, $t) {
         //the constructor of the class Personage is called.
         parent::__construct($n, $v,$vmax, $p, $a, $d, $vi, $t);
+    }
+    public function getnom(){
+        return $this->Nom;
     }
 
     public function Attaque($enemy){
@@ -107,12 +144,18 @@ class villain extends Personage{
         //attack the enemy
         print("L'enemy t'attaque avec ". $this->technique[key($this->technique)] ."\n");
         print("Tu prend ".$enemy->prendredegat($this->Attack*key($this->technique))." de degats.\n");
+        sleep(2);
+        popen('cls','w');
+        $enemy->arreterdesedeffendre();
         reset($this->technique);
     }
 
     //stop defending
     public function arreterdesedeffendre(){
-        $this->sedefendre=false;
+        if ($this->sedefendre==true){
+            $this->Defence= $this->Defence/2;
+            $this->sedefendre=false;
+        }
     }
 
     public function prendredegat($attackEnemy){
@@ -140,6 +183,11 @@ class villain extends Personage{
     public function Defendre(){
         //increase the defence
         $this->Defence=$this->Defence*2;
+        $this->sedefendre=true;
+        print("L'enemi se defend.\n");
+        print("Defence X2!\n");
+        sleep(2);
+        popen('cls','w');
     }
     public function afficherStat(){
         print("Voici les statistique de ".$this->Nom.":\n");
@@ -147,18 +195,34 @@ class villain extends Personage{
         print("Attaque:".$this->Attack."\n");
         print("Defence:".$this->Defence."\n");
         print("Vitesse:".$this->Vitesse."\n");
-        print("Vie:".$this->Vie."/".$this->VieMax."\n");
+        print("Vie Max: ".$this->VieMax."\n");
+        print("Barre de vie :[");
+        for ($i=0; $i <round($this->Vie/$this->VieMax*20) ; $i++) { 
+            print("|");
+        }
+        for ($i=0; $i <20-round($this->Vie/$this->VieMax*20) ; $i++) { 
+            print(".");
+        }
+        print("]\n");
     }
     public function getnbrdevictoire(){
         return $this->nbrdevictoire;
     }
+
+    public function Fullvie(){
+        $this->Vie= $this->VieMax;
+    }
 }
 
 function combat($attaquant, $cible){
+    print("Vous tomber sur ".$cible->getnom().".\n");
+    sleep(2);
+    popen('cls','w');
     while ($attaquant->mourrir() && $cible->mourrir()) {
        $attaquant->afficherStat();
        $cible->afficherStat();
        $reponse = readline("Que souhaiter vous faire :\n 1-attacker\n 2-Defendre\n");
+       $choixEnemy=random_int(1,4);
        popen('cls','w');
        //Check if attack or defend
        if ($reponse == 1){
@@ -169,8 +233,13 @@ function combat($attaquant, $cible){
                    $cible->Attaque($attaquant);
                 }
             }
-            else{
-                $cible->Attaque($attaquant);
+            else{               
+                if($choixEnemy!=4){
+                    $cible->Attaque($attaquant);
+                }
+                else{
+                    $cible->Defendre();
+                }
                 if ($attaquant->mourrir()) {
                    $attaquant->Attaque($cible);
                }
@@ -181,23 +250,47 @@ function combat($attaquant, $cible){
             $cible->Attaque($attaquant);
         }
         else{
-            print("Tu n'as pas réussit a choisir.\n");
+            print("Tu n'as pas reussit a choisir.\n");
             $cible->Attaque($attaquant);
         }
-   }
-   //check who win
-   if ($attaquant->mourrir()){
-        print("Vous avez vaincu l'enemy bravo!");
-        return true;
-   }
-   else{
-        print("Vous avez perdu...");
-        return false;
-   }
+        //check who win
+    }
 }
-
-$hero = new hero("Carote",100,100,10,10,10,10,[1=>"coup de poing",2=>"coup de pied",3=>"coup de boule"]);
-$villain = new villain("Frigidaire",50,50,5,5,5,5,[1=>"coup de poing",2=>"coup de pied",3=>"coup de boule"]);
-
-
-combat($hero,$villain);
+popen('cls','w');
+// Vie,Viemax,Puissance,attack,defence,vitesse,technique
+$hero = new hero("Goku",100,100,140,25,10,10,[1=>"coup de poing",2=>"coup de pied",3=>"coup de boule"]);
+$villain1 = new villain("raditz",105,105,150,25,10,10,[1=>"coup de poing",2=>"coup de pied",3=>"coup de boule"]);
+$villain2 = new villain("Freezer",150,150,150,20,9,15,[1=>"coup de poing",2=>"coup de pied",3=>"coup de boule",4=>"Lazer"]);
+$villain3 = new villain("Super Buu",180,180,160,40,30,18,[1=>"coup de poing",2=>"coup de pied",3=>"coup de boule",9=>"transformation en bonbon"]);
+$villain4 = new villain("Kid Buu",200,200,200,45,25,27,[1=>"coup de poing",2=>"coup de pied",3=>"coup de boule",10=>"boule d'energie"]);
+$villains=[$villain1,$villain2,$villain3,$villain4];
+print("Vous vous balader tranquilement quand tout d'un coup...\n");
+sleep(2);
+popen('cls','w');
+while($hero->getnbrdevictoire()<4){
+    combat($hero,$villains[$hero->getnbrdevictoire()]);
+    if ($hero->mourrir()){
+        print("Vous avez vaincu l'enemy bravo!\n");
+        $hero->victoire();
+        sleep(2);
+        if($hero->getnbrdevictoire()==1){
+            $hero->addtechnique([5=>"Kamehameha"]);
+            print("Nouvel attack appris -> Kamehameha!\n");
+            sleep(2);
+            popen('cls','w');
+        }
+        if($hero->getnbrdevictoire()==3){
+            $hero->addtechnique([8=>"makankosappo"]);
+            print("Nouvel attack appris ->makankosappo!\n");
+            sleep(2);
+            popen('cls','w');
+        }
+    }
+    else{
+        print("Vous avez perdu...\n");
+        sleep(2);
+        popen('cls','w');
+    }
+    $hero->Fullvie();
+    $villains[$hero->getnbrdevictoire()]->Fullvie();
+}
